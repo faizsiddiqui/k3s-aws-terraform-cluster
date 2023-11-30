1cbf1ab8219ba1314462671d66d3c3f7f5a0c6ac
Faiz Siddiqui <faiz.siddiqui@gomotive.com>
2023-11-30 13:56:45 +0530

    Recreate kubeconfig secret

--- k3s_cluster/secrets.tf
+++ k3s_cluster/secrets.tf
@@ -2,6 +2,10 @@ resource "aws_secretsmanager_secret" "kubeconfig_secret" {
   name        = local.kubeconfig_secret_name
   description = "Kubeconfig k3s. Cluster name: ${var.cluster_name}, environment: ${var.environment}"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags = merge(
     local.global_tags,
     {
