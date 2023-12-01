5de5532a2cd0851b1b0f6e63825b23461f9798ee
Faiz Siddiqui <faiz.siddiqui@gomotive.com>
2023-12-01 16:01:03 +0530

    change

--- k3s_cluster/secrets.tf
+++ k3s_cluster/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "kubeconfig_secret" {
-  name        = "${local.kubeconfig_secret_name}"
+  name        = local.kubeconfig_secret_name
   description = "Kubeconfig k3s. Cluster name: ${var.cluster_name}, environment: ${var.environment}"
 
   lifecycle {
